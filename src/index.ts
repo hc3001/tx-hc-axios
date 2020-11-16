18a3678507606de312d796cae5126245abe59acd
hcc <huangcheng3576@gmail.com>
2020-11-16 20:12:36 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { AxiosRequestConfig } from './types/index'
+
+function axios(config: AxiosRequestConfig) {
+    
+}
+
+export default axios
---
+++ src/xhr.ts
@@ -0,0 +1,8 @@
+import { AxiosRequestConfig } from './types/index'
+
+function xhr(config: AxiosRequestConfig) {
+    const { data = null, url, method = 'get'} = config
+    const re
+}
+
+export default xhr
